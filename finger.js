9eabb7971a1a68797e73ffde203d443e940f0015
Juan Gonzalez <jugonzal@gmail.com>
2018-11-26 15:58:37 -0500

    reading arguments from command line

---
+++ finger.js
@@ -0,0 +1,18 @@
+// - Pick two numbers between 6 and 10
+
+var args = process.argv;
+console.log(args);
+
+//  - Mapping numbers to each hand following these order:
+// #  - 6 -> thumb
+// #  - 7 -> index
+// #  - 8 -> middle
+// #  - 9 -> ringer
+// #  - 10 -> pinky
+// #- Group all fingers that are up to the "mapped" finger
+// #- Repeat for the second number on the other hand
+// #- Join the mapped fingers, count them and multiply by 10
+// #- Take the leftover fingers from each hand and multiply left by right
+// #- Add up the mapped x 10 value with the leftover multiplication
+// #- Done... this should be the result
+
